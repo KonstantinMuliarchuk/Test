ecece3b47fc990f3f3e0fb2b6c210c50fa7b5526
Мулярчук Константин <kotipodgmail.com@MacBook-Air-Mularcuk.local>
2017-05-11 15:09:36 +0300

    Initial Commit

---
+++ Test/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Test
+//
+//  Created by Мулярчук Константин on 11.05.17.
+//  Copyright © 2017 Мулярчук Константин. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
